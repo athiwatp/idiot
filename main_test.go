dfd97e2b3433b61ea534cde1e7e25b682baaecb8
athiwat.p5@gmail.com <athiwat.p5@gmail.com>
2019-09-06 01:39:17 +0700

    init

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestIdiot_Helper(t *testing.T) {
+
+	t.Run("Happy - validate string", func(t *testing.T) {
+
+		main()
+
+		//assert.NotNil(t, result)
+		//assert.NoError(t, err)
+	})
+
+}
